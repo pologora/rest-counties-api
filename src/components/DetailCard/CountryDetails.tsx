3423c1a50371fecaf6c1262577c53e56efbc434f
Oleksandr <lysakova@yahoo.com>
2023-12-03 16:58:42 +0100

    Add desktop design to CountryDetails component

--- src/components/DetailCard/CountryDetails.tsx
+++ src/components/DetailCard/CountryDetails.tsx
@@ -16,8 +16,6 @@ const CountryDetails = () => {
   const { countries } = useCountriesContext();
   const [data, setData] = useState<CountryType>();
 
-  console.log(data);
-
   const navigate = useNavigate();
 
   const handleGoBack = () => {
@@ -57,6 +55,7 @@ const CountryDetails = () => {
       {item}
     </li>
   ));
+
   const languagesList = getLanguages(languages).map((item) => (
     <li className='country-details__languages-list-item' key={item}>
       {item}
@@ -86,70 +85,95 @@ const CountryDetails = () => {
   ));
 
   return (
-    <article className='country-details'>
+    <article className='country-details container'>
       <button className='country-details__back-button' onClick={handleGoBack}>
         <IoIosArrowRoundBack className='country-details__back-button-icon' />
         <span className='country-details__back-button-text'>Back</span>
       </button>
-      <div className='country-details__flag-container'>
-        <img src={svg} alt={alt} className='country-details__flag' />
-      </div>
-      <h2 className='country-details__name'>{common}</h2>
-      <div className='country-details__properties-list'>
-        <div className='country-details__properties-list-main'>
-          <p className='country-details__native-name country-details__property'>
-            <span className='country-details__property-title'>
-              Native Name:
-            </span>
-            <span className='country-details__property-value'>
-              {nativeCommonName}
-            </span>
-          </p>
-          <p className='country-details__population country-details__property'>
-            <span className='country-details__property-title'>Population:</span>
-            <span className='country-details__property-value'>
-              {population.toLocaleString('en-US')}
-            </span>
-          </p>
-          <p className='country-details__region country-details__property'>
-            <span className='country-details__property-title '>Region:</span>
-            <span className='country-details__property-value'>{region}</span>
-          </p>
-          <p className='country-details__sub-region country-details__property'>
-            <span className='country-details__property-title'>Sub Region:</span>
-            <span className='country-details__property-value'>{subregion}</span>
-          </p>
-          <p className='country-details__capital country-details__property'>
-            <span className='country-details__property-title '>Capital:</span>
-            <span className='country-details__property-value'>{capital}</span>
-          </p>
+      <div className='country-details__content'>
+        <div className='country-details__flag-container'>
+          <img src={svg} alt={alt} className='country-details__flag' />
         </div>
-        <div className='country-details__properties-list-secondary'>
-          <p className='country-details__domain country-details__property'>
-            <span className='country-details__property-title'>
-              Top Level Domain:
-            </span>
-            <ul className='country-details__domain-list'>{domainList}</ul>
-          </p>
-          <p className='country-details__currencies country-details__property'>
-            <span className='country-details__property-title'>Currencies:</span>
-            <ul className='country-details__currencies-list'>
-              {currenciesList}
+
+        <div className='country-details__properties-list'>
+          <h2 className='country-details__name'>{common}</h2>
+          <div className='country-details__properties-container'>
+            <div className='country-details__properties-list-main'>
+              <p className='country-details__native-name country-details__property'>
+                <span className='country-details__property-title'>
+                  Native Name:
+                </span>
+                <span className='country-details__property-value'>
+                  {nativeCommonName}
+                </span>
+              </p>
+              <p className='country-details__population country-details__property'>
+                <span className='country-details__property-title'>
+                  Population:
+                </span>
+                <span className='country-details__property-value'>
+                  {population.toLocaleString('en-US')}
+                </span>
+              </p>
+              <p className='country-details__region country-details__property'>
+                <span className='country-details__property-title '>
+                  Region:
+                </span>
+                <span className='country-details__property-value'>
+                  {region}
+                </span>
+              </p>
+              <p className='country-details__sub-region country-details__property'>
+                <span className='country-details__property-title'>
+                  Sub Region:
+                </span>
+                <span className='country-details__property-value'>
+                  {subregion}
+                </span>
+              </p>
+              <p className='country-details__capital country-details__property'>
+                <span className='country-details__property-title '>
+                  Capital:
+                </span>
+                <span className='country-details__property-value'>
+                  {capital}
+                </span>
+              </p>
+            </div>
+            <div className='country-details__properties-list-secondary'>
+              <p className='country-details__domain country-details__property'>
+                <span className='country-details__property-title'>
+                  Top Level Domain:
+                </span>
+                <ul className='country-details__domain-list'>{domainList}</ul>
+              </p>
+              <p className='country-details__currencies country-details__property'>
+                <span className='country-details__property-title'>
+                  Currencies:
+                </span>
+                <ul className='country-details__currencies-list'>
+                  {currenciesList}
+                </ul>
+              </p>
+              <p className='country-details__languages country-details__property'>
+                <span className='country-details__property-title'>
+                  Languages:
+                </span>
+                <ul className='country-details__languages-list'>
+                  {languagesList}
+                </ul>
+              </p>
+            </div>
+          </div>
+          <div className='country-details__border-country-list-container'>
+            <h3 className='country-details__border-country-list-title'>
+              Border Countries:
+            </h3>
+            <ul className='country-details__border-country-list'>
+              {borderCountriesListElements}
             </ul>
-          </p>
-          <p className='country-details__languages country-details__property'>
-            <span className='country-details__property-title'>Languages:</span>
-            <ul className='country-details__languages-list'>{languagesList}</ul>
-          </p>
+          </div>
         </div>
-      </div>
-      <div className='country-details__border-country-list-container'>
-        <h3 className='country-details__border-country-list-title'>
-          Border Countries:
-        </h3>
-        <ul className='country-details__border-country-list'>
-          {borderCountriesListElements}
-        </ul>
       </div>
     </article>
   );
